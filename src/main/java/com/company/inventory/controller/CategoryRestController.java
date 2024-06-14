254eae9662b7dff451a27a188df06c797a233974
MarioDev <mherreraalmira@gmail.com>
2024-06-14 02:07:37 -0600

    Add method find category by id

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -3,6 +3,7 @@ package com.company.inventory.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +17,28 @@ public class CategoryRestController {
 	@Autowired
 	private ICategoryService service;
 	
+	/**
+	 * Method to search all categories
+	 * 
+	 * @return ResponseEntity<CategoryResponseRest>
+	 */
 	@GetMapping("/categories")
 	public ResponseEntity<CategoryResponseRest> searchCategories() {
 		
 		ResponseEntity<CategoryResponseRest> response = service.search();
+		return response;
+	}
+	
+	/**
+	 * Method to search a category by id
+	 * 
+	 * @param id
+	 * @return ResponseEntity<CategoryResponseRest>
+	 */
+	@GetMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> searchCategoryById(@PathVariable Long id) {
+
+		ResponseEntity<CategoryResponseRest> response = service.searchById(id);
 		return response;
 	}
 }
